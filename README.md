3cad5e488fceb19347b700b9f4a4779f1f3e47de
Rayzr522 <rayzr522@gmail.com>
2017-01-23 10:15:53 -0500

    Added /fixworld command.
    Hopefully the final commit!

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ChunkFixer
-Fixes a problem with loaded chunks.
+**ChunkFixer** is a simple plugin I made in 5 minutes to help a guy out with a problem in his server where 5k chunks were loaded and weren't unloading. Weird. 
+
+## Usage
+
+### `/fixworld <world>`
+* Permission: `ChunkFixer.main`
+
+That's it! Super simple (just iterates over loaded chunks and attempts to unload them).
--- src/main/java/com/rayzr522/chunkfixer/ChunkFixer.java
+++ src/main/java/com/rayzr522/chunkfixer/ChunkFixer.java
@@ -1,12 +1,41 @@
 package com.rayzr522.chunkfixer;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChunkFixer extends JavaPlugin {
-    
+
     @Override
     public void onEnable() {
-        
+
     }
-    
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            return false;
+        }
+
+        World world = Bukkit.getWorld(args[0]);
+        if (world == null) {
+            sender.sendMessage(ChatColor.RED + "That world does not exist!");
+            return false;
+        }
+
+        for (Chunk c : world.getLoadedChunks()) {
+            if (!c.unload()) {
+                sender.sendMessage(ChatColor.RED + "Failed to unload chunk at " + c.getX() + "," + c.getZ());
+            }
+        }
+
+        sender.sendMessage(ChatColor.GREEN + "World has been repaired.");
+
+        return true;
+    }
+
 }
